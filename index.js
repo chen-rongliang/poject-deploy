eb4e1149a1f9c8b970b42d35cefc63448ca71448
chen-rongliang <chen-rongliang@qq.com>
2018-08-13 11:27:30 +0800

    首次提交

---
+++ index.js
@@ -0,0 +1,23 @@
+/*
+ * 模块入口
+ */
+
+'use strict';
+
+const path = require('path')
+const server = require(path.resolve(__dirname, 'lib/server'))
+const rf = require(path.resolve(__dirname, 'lib/rf'))
+
+// 接收命令行相关参数
+var command = process.argv.slice(2) || 'prod'
+var depConf = require(path.resolve('.', 'deploy-conf'))[command]
+
+// 遍历文件
+let files = rf(depConf.form)
+
+// 开始传输流程
+server({
+    receiver: depConf.receiver,
+    to: depConf.to,
+    files
+})
