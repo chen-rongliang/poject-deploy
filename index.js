bcb8bb65e03abb5274f63cb5b159d91fcdfb620c
chen-rongliang <chen-rongliang@qq.com>
2019-10-22 17:35:51 +0800

    版本批量上传 & 分片上传实现

--- index.js
+++ index.js
@@ -3,11 +3,13 @@
  */
 
 'use strict';
-
+const fs = require('fs')
 const path = require('path')
 const md5 = require('js-md5')
 const server = require(path.resolve(__dirname, 'lib/server'))
+const slices = require(path.resolve(__dirname, 'lib/slices'))
 const rf = require(path.resolve(__dirname, 'lib/rf'))
+const targz = require(path.resolve(__dirname, 'lib/targz'))
 
 // 接收命令行相关参数
 var command = process.argv.slice(2)
@@ -23,8 +25,16 @@ if (depConf) {
     if (!depConf.receiver) {
         console.error('receiver is required!')
     } else {
-        // 遍历文件
-        let files = rf(depConf.form || '.', depConf.ignore || [], depConf.scriptTag)
+        let def
+        if (depConf.zip) {
+            // 如果有zip属性, 则表示把form目录内文件压缩，发送压缩包
+            let to = depConf.to
+            depConf.to = '/'
+            def = targz(depConf.form, to)
+        } else {
+            // 没有，则遍历文件
+            def = rf(depConf.form || '.', depConf.ignore || [], depConf.scriptTag)
+        }
 
         // 如果有script属性
         if (depConf.script) {
@@ -32,19 +42,40 @@ if (depConf) {
             delete depConf.script
         }
 
-        // 找到文件了
-        if (files.length) {
-            // 开始传输流程
-            console.log('start upload => ' + command)
-            server({
-                receiver: depConf.receiver,
-                to: depConf.to || '/',
-                data: depConf.data || {},
-                files
-            })
-        } else {
-            console.log('no file.')
-        }
+        def.then(files => {
+
+            // 找到文件了
+            if (files.length) {
+                // 开始传输流程
+                console.log('start upload => ' + command)
+                // gzip包超过1M大小的，用分片上传
+                if(depConf.zip && files[0].size > 1048576) {
+                    slices({
+                        receiver: depConf.receiver,
+                        to: depConf.to || '/',
+                        data: depConf.data || {},
+                        files,
+                        callback: () => {
+                            // 删除临时压缩文件
+                            fs.unlink(files.pop().path, () => {})
+                        }
+                    })
+                } else {
+                    server({
+                        receiver: depConf.receiver,
+                        to: depConf.to || '/',
+                        data: depConf.data || {},
+                        files
+                    })
+                }
+            } else {
+                console.log('no file.')
+            }
+
+        }).catch(err => {
+            console.log(err)
+        })
+
     }
 } else {
     console.error('undefined command!')
